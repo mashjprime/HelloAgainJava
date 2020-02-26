27936a6d7fa8e028817c341ce240f08e56ab15d7
Mash J Prime <mashjprime@gmail.com>
2020-02-26 22:29:20 +0800

    Moved message to class

--- HelloAgainJava/src/main.java
+++ HelloAgainJava/src/main.java
@@ -3,7 +3,12 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		test();
+	}
+	
+	public static void test() {
 		System.out.println("Hello World");
+		
 	}
 
 }
