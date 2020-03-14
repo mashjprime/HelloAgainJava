d7beeeb0bd7d8d906e27002a5cf86a482a6912c2
Mash J Prime <mashjprime@gmail.com>
2020-03-14 11:44:27 +0800

    Refactoring Complete

--- HelloAgainJava/src/TuteLab01.java
+++ HelloAgainJava/src/TuteLab01.java
@@ -8,104 +8,62 @@ import java.util.Scanner;
 //       consider the use of static variables as well and use printf where appropriate
 //
 // Ohm's law V=IR .. https://en.wikipedia.org/wiki/Ohm%27s_law
-public class TuteLab01
-{
-    static double volts[];
-    static double amps[];
-    static double resistance[];
-    static Scanner scanner = new Scanner(System.in);
-    static double v;
-    static double i;
-    static double r;
-    static boolean doAgain;
-    static double vavg;
-    static double vmax;
-    static double vmin;
-    static double iavg;
-    static double imax;
-    static double imin;
-    static double ravg;
-    static double rmax;
-    static double rmin;
+public class TuteLab01 {
+    private static Scanner scanner = new Scanner(System.in);
 
-    public static void TuteLab01()
-    {
-        volts = new double[3];
-        amps = new double[3];
-        resistance = new double[3];
+    static void tuteLab01() {
+        final int TOTAL_INPUT = 3;
+        double[] volts = new double[TOTAL_INPUT];
+        double[] amps = new double[TOTAL_INPUT];
+        double[] resistance = new double[TOTAL_INPUT];
 
-        scanner = new Scanner(System.in);
+        System.out.println("Enter " + TOTAL_INPUT + " pairs of V and I");
 
-        System.out.println("Enter 3 pairs of V and I");
+        for (int count = 0; count < TOTAL_INPUT; count++) {
+            volts[count] = getInputValues("Enter Voltage (V) in volts: ");
+            amps[count] = getInputValues("Enter Current (I) in amperes: ");
+            resistance[count] = volts[count] / amps[count];
 
-        for (int count = 0; count < 3; count++)
-        {
-            doAgain = true;
-            double num = 0;
-
-            do
-            {
-                try
-                {
-                    System.out.print("Enter Voltage (V) in volts: ");
-                    // parseDouble() will "throw" an exception if a number is not
-                    // entered. Execution then immediately moves to catch block
-                    num = Double.parseDouble(scanner.nextLine());
-                    doAgain = false;
-                }
-                catch (NumberFormatException nfe)
-                {
-                    // we don't really need to do anything here since we will just loop and try again, but could.
-                }
-            }
-            while (doAgain == true);
-
-            v = num;
-            volts[count] = v;
-
-            doAgain = true;
-
-            do
-            {
-                try
-                {
-                    System.out.print("Enter Current (I) in amperes: ");
-                    // parseDouble() will "throw" an exception if a number is not
-                    // entered. Execution then immediately moves to catch block
-                    num = Double.parseDouble(scanner.nextLine());
-                    doAgain = false;
-                }
-                catch (NumberFormatException nfe)
-                {
-                    // we don't really need to do anything here since we will just loop and try again, but could.
-                }
-            }
-            while (doAgain == true);
-
-            i = num;
-            amps[count] = i;
-            r = v / i;
-            resistance[count] = r;
-
-            System.out.println("Resistance R is " + r + " ohms\n");
+            System.out.println("Resistance R is " + resistance[count] + " ohms\n");
         }
 
-        vavg = (volts[0] + volts[1] + volts[2]) / 3;
-        vmax = Math.max(Math.max(volts[0], volts[1]), volts[2]);
-        vmin = Math.min(Math.min(volts[0], volts[1]), volts[2]);
+        System.out.println(calculateOutput("Volts", volts));
+        System.out.println(calculateOutput("Amps", amps));
+        System.out.println(calculateOutput("Resistance", resistance));
+    }
 
-        System.out.println("V (Volts) .. Avg=" + vavg + ", Max=" + vmax + ", Min=" + vmin);
+    private static double getInputValues(String message) {
+        double num = 0;
+        boolean doAgain = true;
+        do {
+            try {
+                System.out.print(message);
+                num = Double.parseDouble(scanner.nextLine());
+                doAgain = false;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Input error");
+            }
+        }
+        while (doAgain);
 
-        iavg = (amps[0] + amps[1] + amps[2]) / 3;
-        imax = Math.max(Math.max(amps[0], amps[1]), amps[2]);
-        imin = Math.min(Math.min(amps[0], amps[1]), amps[2]);
+        return num;
+    }
 
-        System.out.println("I (Amperes) .. Avg=" + iavg + ", Max=" + imax + ", Min=" + imin);
+    private static String calculateOutput(String measurement, double[] num) {
 
-        ravg = (resistance[0] + resistance[1] + resistance[2]) / 3;
-        rmax = Math.max(Math.max(resistance[0], resistance[1]), resistance[2]);
-        rmin = Math.min(Math.min(resistance[0], resistance[1]), resistance[2]);
-
-        System.out.println("R (Ohms) .. Avg=" + ravg + ", Max=" + rmax + ", Min=" + rmin);
+        double total = 0;
+        double max = num[0];
+        double min = num[0];
+        for (double i : num) {
+            total += i;
+            if (i > max) {
+                max = i;
+            }
+            if(i<min) {
+                min = i;
+            }
+        }
+        double avg = total / num.length;
+        return (measurement + ".. Avg=" + avg + ", Max=" + max + ", Min=" + min);
     }
 }
--- HelloAgainJava/src/main.java
+++ HelloAgainJava/src/main.java
@@ -2,7 +2,7 @@ public class main {
 
     public static void main(String[] args) {
         // DateTest.test();
-		TuteLab01.TuteLab01();
+		TuteLab01.tuteLab01();
     }
 
 
